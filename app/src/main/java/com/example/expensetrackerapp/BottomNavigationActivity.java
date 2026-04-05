6bb5a7acfbf57d764af10ff29ec4a7108a4baf00
iDevMirza <dev.mirzamahmud@gmail.com>
2026-04-06 00:07:44 +0100

    complete design of the bottom nav activity

--- app/src/main/java/com/example/expensetrackerapp/BottomNavigationActivity.java
+++ app/src/main/java/com/example/expensetrackerapp/BottomNavigationActivity.java
@@ -1,5 +1,4 @@
 package com.example.expensetrackerapp;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -9,49 +8,73 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.example.expensetrackerapp.databinding.ActivityBottomNavigationBinding;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class BottomNavigationActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
-
-    ActivityBottomNavigationBinding binding;
+    private int currentSelectedItemId = R.id.home_menu_id;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
-        binding = ActivityBottomNavigationBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        binding.bottomNavigationView.setOnItemSelectedListener(this);
-
+        setContentView(R.layout.activity_bottom_navigation);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            currentSelectedItemId = R.id.home_menu_id;
+            loadFragment(new HomeFragment());
+            bottomNavigationView.setSelectedItemId(R.id.home_menu_id);
+        } else {
+            currentSelectedItemId = bottomNavigationView.getSelectedItemId();
+        }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.home_menu_id){
+        int itemId = item.getItemId();
 
+        // Prevent reloading same fragment
+        if (itemId == currentSelectedItemId) {
+            return true;
         }
 
-        if(item.getItemId() == R.id.add_menu_id){
+        Fragment selectedFragment = null;
 
+        if(itemId == R.id.home_menu_id){
+            selectedFragment = new HomeFragment();
+        } else if(itemId == R.id.add_menu_id){
+            selectedFragment = new AddFragment();
+        } else if(itemId == R.id.history_menu_id){
+            selectedFragment = new HistoryFragment();
+        } else if(itemId == R.id.insights_menu_id){
+            selectedFragment = new InsightsFragment();
         }
 
-        if(item.getItemId() == R.id.history_menu_id){
-
+        if (selectedFragment != null) {
+            currentSelectedItemId = itemId;
+            loadFragment(selectedFragment);
+            return true;
         }
 
-        if(item.getItemId() == R.id.insights_menu_id){
+        return false;
+    }
 
-        }
-
-        return true;
+    private void loadFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout_id, fragment);
+        fragmentTransaction.commit();
     }
 }
